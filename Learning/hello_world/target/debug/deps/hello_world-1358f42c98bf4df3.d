4a2946cb58ef1f216fb1dee3ac98759c79f1b114
Ithaca <2576080342@qq.com>
2026-01-16 13:33:54 +0800

    tryagain

---
+++ Learning/hello_world/target/debug/deps/hello_world-1358f42c98bf4df3.d
@@ -0,0 +1,5 @@
+/home/Ithaca/Rust/Learning/hello_world/target/debug/deps/hello_world-1358f42c98bf4df3.d: src/main.rs
+
+/home/Ithaca/Rust/Learning/hello_world/target/debug/deps/libhello_world-1358f42c98bf4df3.rmeta: src/main.rs
+
+src/main.rs:
---
+++ Learning/hello_world/target/debug/deps/hello_world-e5e403fc6963d888.d
@@ -0,0 +1,5 @@
+/home/Ithaca/Rust/Learning/hello_world/target/debug/deps/hello_world-e5e403fc6963d888.d: src/main.rs
+
+/home/Ithaca/Rust/Learning/hello_world/target/debug/deps/libhello_world-e5e403fc6963d888.rmeta: src/main.rs
+
+src/main.rs:
